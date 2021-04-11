e125f7e07b190aebbd794b4608a888709e3dbe3c
vtejaeta <vtejaeta@gmail.com>
2021-04-11 19:06:39 +0530

    Reafctored code

--- src/Pet.js
+++ src/Pet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Pet({ name, animal, breed, media, location, id }) {
-  let hero = "http://placeorgi.com/300/300";
+  let hero = "http://placecorgi.com/300/300";
   if (media.length) {
     hero = media[0].small;
   }
